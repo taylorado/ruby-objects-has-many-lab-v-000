4b050e870ead91a97aca72277eaefff11c6c0b67
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 17:44:44 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author
   attr_accessor :name, :posts
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
@@ -13,20 +15,18 @@ class Author
   def add_post(post)
     post.author = self
     @posts << post
+    @@post_count+=1
   end
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     post.author = self
     @posts << post
+    @@post_count+=1
   end
 
-  def self.post_count
-    if @posts = nil
-      return nil
-    else
-      return @posts.count
-    end
+  def post_count
+     @@post_count
 
 
   end
