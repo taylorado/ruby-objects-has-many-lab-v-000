d2fa1aad8bb23e837c4a4b66fa08592e2504ea27
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 17:19:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 class Artist
   attr_accessor :name
 
+  @@song_count=0
+
   def initialize(name)
     @name = name
     @songs = []
@@ -8,12 +10,14 @@ class Artist
 
   def add_song(song)
     @songs << song
+    @@songs += 1
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
+    @@songs += 1
     song.artist = self
   end
 
